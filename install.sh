8b9db14777c608057249f3d1321c8b1c21e1ef14
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-08-24 15:12:37 -0300

    update-install

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ chmod 777 /home/abner/sqlterrama2postgis/csv
 chmod 777 /home/abner/sqlterrama2postgis/csv/an_clim_mensal.csv
 chmod 777 /home/abner/sqlterrama2postgis/csv/an_mensal.csv
 chmod 777 /home/abner/sqlterrama2postgis/csv/municipios_brasil.csv
-createdb -h 0.0.0.0 -p 2303 -U postgres data "Dados de preciptação"
+createdb -h 0.0.0.0 -p 2303 -U postgres chuva "Dados de preciptação"
 psql -h 0.0.0.0 -p 2303 -U postgres -c "CREATE USER chuva WITH PASSWORD 'chuva'"
-psql -h 0.0.0.0 -p 2303 -U postgres -d data -c "CREATE EXTENSION postgis"
-psql -h 0.0.0.0 -p 2303 -U postgres -d data -f sql/create_table_cities.sql
-psql -h 0.0.0.0 -p 2303 -U postgres -d data -f sql/create_table_analysis_clim.sql
-psql -h 0.0.0.0 -p 2303 -U postgres -d data -f sql/create_table_analysis_month.sql
+psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -c "CREATE EXTENSION postgis"
+psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -f sql/create_table_cities.sql
+psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -f sql/create_table_analysis_clim.sql
+psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -f sql/create_table_analysis_month.sql
