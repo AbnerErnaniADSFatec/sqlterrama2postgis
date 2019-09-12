ee39fa01561a7cf73876d84b92323e5713340dd3
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2019-09-12 11:37:41 -0300

    update-install

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 docker pull mdillon/postgis
 docker container run --name postgis -p 2303:5432 -d mdillon/postgis
-chmod 777 /home/abner/sqlterrama2postgis/csv
-chmod 777 /home/abner/sqlterrama2postgis/csv/an_clim_mensal.csv
-chmod 777 /home/abner/sqlterrama2postgis/csv/an_mensal.csv
-chmod 777 /home/abner/sqlterrama2postgis/csv/municipios_brasil.csv
+chmod 777 -R /home/abner/sqlterrama2postgis/csv
 createdb -h 0.0.0.0 -p 2303 -U postgres chuva "Dados de preciptação"
 psql -h 0.0.0.0 -p 2303 -U postgres -c "CREATE USER chuva WITH PASSWORD 'chuva'"
 psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -c "CREATE EXTENSION postgis"
