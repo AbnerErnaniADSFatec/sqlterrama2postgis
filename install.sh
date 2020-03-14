595b668f406c4aaa131c2804ee5ff722b2160307
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2020-03-13 21:53:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ docker pull mdillon/postgis
 docker container run --name postgis -p 2303:5432 -d mdillon/postgis
 chmod 777 -R /home/abner/sqlterrama2postgis/csv
 createdb -h 0.0.0.0 -p 2303 -U postgres chuva "Dados de preciptação"
-psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -f sql/create_tables.sql
+psql -h 0.0.0.0 -p 2303 -U postgres -d chuva -f sql/create-tables.sql
